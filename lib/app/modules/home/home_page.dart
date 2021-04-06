264e967b48cb3071e7d21574f0731385e5a25b8d
Bruno <53619830+sklorde@users.noreply.github.com>
2021-04-05 23:52:40 -0300

    :+1: implements colors using class

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:getCrypto/app/utils/getcrypto_colors.dart';
 import 'package:getCrypto/app/widgets/card_crypto/card_crypto_widget.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'home_controller.dart';
@@ -17,9 +18,9 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
       body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        color: Color.fromRGBO(11, 20, 37, 1),
+        color: GetCryptoColors.primary,
         child: Padding(
-          padding: EdgeInsets.only(left: 20, right: 20, bottom: 20, top: 20),
+          padding: EdgeInsets.all(20),
           child: Column(
             children: [
               Column(
@@ -49,35 +50,37 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
                     ],
                   ),
                   Divider(
-                    color: Color.fromRGBO(41, 55, 76, 1).withOpacity(0.42),
+                    color: GetCryptoColors.secundary,
                   ),
                 ],
               ),
-              Expanded(child: Observer(builder: (_) {
-                return SmartRefresher(
-                  controller: controller.refreshController,
-                  onLoading: controller.onLoad,
-                  onRefresh: controller.onRefresh,
-                  enablePullDown: false,
-                  enablePullUp: false,
-                  child: ListView.separated(
-                    physics: BouncingScrollPhysics(),
-                    itemCount: controller.cryptos.length,
-                    itemBuilder: (context, index) {
-                      final crypto = controller.cryptos[index];
+              Expanded(
+                child: Observer(
+                  builder: (_) {
+                    return SmartRefresher(
+                      controller: controller.refreshController,
+                      onLoading: controller.onLoad,
+                      onRefresh: controller.onRefresh,
+                      enablePullDown: false,
+                      enablePullUp: false,
+                      child: ListView.separated(
+                        physics: BouncingScrollPhysics(),
+                        itemCount: controller.cryptos.length,
+                        itemBuilder: (context, index) {
+                          final crypto = controller.cryptos[index];
 
-                      return CardCryptoWidget(
-                        crypto: crypto,
-                      );
-                    },
-                    separatorBuilder: (context, index) {
-                      return Padding(
-                        padding: EdgeInsets.only(top: 15),
-                      );
-                    },
-                  ),
-                );
-              })),
+                          return CardCryptoWidget(
+                            crypto: crypto,
+                          );
+                        },
+                        separatorBuilder: (context, index) {
+                          return Padding(padding: EdgeInsets.only(top: 15));
+                        },
+                      ),
+                    );
+                  },
+                ),
+              ),
             ],
           ),
         ),
