017e08b8dbcb0cc4cdd6e55823c7bffe04e99554
Bruno <53619830+sklorde@users.noreply.github.com>
2021-04-05 23:52:20 -0300

    :+1: create color class

---
+++ lib/app/utils/getcrypto_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class GetCryptoColors {
+  static const primary = Color.fromRGBO(11, 20, 37, 1);
+  static const secundary = Color.fromRGBO(41, 55, 76, 42);
+}
