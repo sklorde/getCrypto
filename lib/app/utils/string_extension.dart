7adc39e8e62e23c1d5ef234f0d997b7598c14551
Bruno <53619830+sklorde@users.noreply.github.com>
2021-05-03 20:46:00 -0300

    :+1: function to remove accents

--- lib/app/utils/string_extension.dart
+++ lib/app/utils/string_extension.dart
@@ -1,5 +1,11 @@
+import 'package:diacritic/diacritic.dart';
+
 extension StringExtension on String {
   toURL() {
     return this.toLowerCase().replaceAll(' ', '-');
+  }
+
+  toCompare() {
+    return removeDiacritics(this.toLowerCase());
   }
 }
